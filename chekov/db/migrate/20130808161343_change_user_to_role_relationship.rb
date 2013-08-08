7168bbde3aaa647e7d49551a4e2733dd6bcafd92
cpcoats <cpcoats@gmail.com>
2013-08-08 13:31:40 -0400

    added index to roles_users table to increase speed

--- chekov/db/migrate/20130808161343_change_user_to_role_relationship.rb
+++ chekov/db/migrate/20130808161343_change_user_to_role_relationship.rb
@@ -3,4 +3,6 @@ class ChangeUserToRoleRelationship < ActiveRecord::Migration
   	t.belongs_to :user 
   	t.belongs_to :role
   end
+  add_index :roles_users, :user
+  add_index :roles_users, :role
 end
